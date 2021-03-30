ef35f865319be3ba8a347921f14e7d697abd56f6
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-03-30 10:17:38 +0800

    firebase nodejs server integration done, able to save data to firebase realtime database via nodejs

--- index.js
+++ index.js
@@ -2,16 +2,35 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+var admin = require('firebase-admin');
+var serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://angular-96299.firebaseio.com"
+});
+
 app.use(express.json())
+// app.use()
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.post('/acara/baru', (req, res) => {
-    // res.send('Acara baru!')
-    // res.send("Name: "+req.body.name)
-    // console.log("Name: "+req.body.name)
+    var db = admin.database();
+    var ref = db.ref("/coba");
+    var usersRef = ref.child("users");
+    usersRef.set({
+        alanisawesome: {
+            date_of_birth: "June 23, 1912",
+            full_name: req.body.name
+        },
+        gracehop: {
+            date_of_birth: "December 9, 1906",
+            full_name: "Grace Hopper"
+        }
+    });
 })
 
 app.listen(port, () => {
